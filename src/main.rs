7d5b5c0b1517102db8d3a36651dafd730b07bc20
Maxwell L-T <maxwell.lt@live.com>
2021-03-20 12:37:58 -0400

    Interactive light level setting
    If the connection takes exactly 30 seconds, open port 113
    Output of telnet responses are still not really working,
    seems to end up 1 message behind

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,74 @@
 use telnet::{Telnet, TelnetEvent};
+use anyhow::Result;
+use std::io::{self, Write};
+use std::time::Instant;
 
-fn main() {
-    println!("Connecting...");
-    let mut telnet = Telnet::connect(("10.0.0.73", 23), 256)
-        .expect("Could not connect to Smart Bridge!");
-    println!("Connected");
+fn main() -> Result<()> {
+    let mut telnet: Telnet;
+    {
+        println!("Connecting...");
+        let start = Instant::now();
+        telnet = login("10.0.0.73")?;
+        let duration = start.elapsed();
+        println!("Connected after {} seconds", duration.as_secs());
+    }
 
-    let username = "lutron\n";
-    let password = "integration\n";
-    let command = "#output,3,1,15\n";
-    let mut sent = false;
     loop {
-        println!("Reading...");
-        let event = telnet.read().expect("Read error");
-        println!("Read data");
-        match event {
-            TelnetEvent::Data(buffer) => {
-                println!("{:?}", String::from_utf8_lossy(&buffer));
-                if String::from_utf8_lossy(&buffer).eq("login: ") {
-                    telnet.write(username.as_bytes()).expect("Write error");
-                } else if String::from_utf8_lossy(&buffer).eq("password: ") {
-                    telnet.write(password.as_bytes()).expect("Write error");
-                } else if sent == false {
-                    telnet.write(command.as_bytes()).expect("Write error");
-                    sent = true;
+        print!("Enter a new light level for the switch with ID 3: ");
+        io::stdout().flush()?;
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        match buffer.trim().parse::<f64>() {
+            Ok(new_level) => {
+                println!("You entered {}", new_level);
+                let command = format!("#OUTPUT,3,1,{}\n", buffer.trim());
+                println!("Sending command <{}>...", command.trim());
+                telnet.write(command.as_bytes())?;
+                if let TelnetEvent::Data(response) = telnet.read()? {
+                    let data = String::from_utf8_lossy(&response);
+                    for line in data.lines() {
+                        if line != "GNET> " {
+                            println!("{}", line);
+                        }
+                    }
                 }
             },
-            _ => {}
+            Err(e) => {
+                println!("Invalid input: {}", e);
+            }
+        }
+    }
+}
+
+fn login(address: &str) -> Result<Telnet> {
+    const USERNAME: &str = "lutron\n";
+    const PASSWORD: &str = "integration\n";
+
+    let mut conn: Telnet = Telnet::connect((address, 23), 256)?;
+
+    loop {
+        let evt = conn.read()?;
+        match evt {
+            TelnetEvent::Data(buf) => {
+                let line = String::from_utf8_lossy(&buf);
+                match line.to_string().as_str() {
+                    "login: " => {
+                        conn.write(USERNAME.as_bytes())?;
+                        continue
+                    },
+                    "password: " => {
+                        conn.write(PASSWORD.as_bytes())?;
+                        continue
+                    },
+                    "GNET> " => {
+                        return Ok(conn);
+                    },
+                    _ => {
+                        continue
+                    }
+                }
+            }
+            _ => continue
         }
     }
 }
